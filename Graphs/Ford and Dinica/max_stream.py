10bb4368876a79adb51b4ce3d72abb5f02a4c795
Nhya <tim.beliachenko@gmail.com>
2022-11-15 23:52:30 +0400

    Diaphanous update

--- Graphs/Ford and Dinica/max_stream.py
+++ Graphs/Ford and Dinica/max_stream.py
@@ -17,7 +17,7 @@ graph = [[0, 6, 0, 0, 0, 9],
 
 visited = set()
 
-def dfs(a, b, delta, residual_network):
+def dfs_Falk(a, b, delta, residual_network):
     
     if a == b:
         return delta
@@ -30,7 +30,7 @@ def dfs(a, b, delta, residual_network):
     for neighbour in [node for node in range(len(graph[a])) if graph[a][node] > 0]:
 
         if neighbour not in visited and residual_network[a][neighbour] < graph[a][neighbour]:
-            cur_delta = dfs(neighbour, b, min(delta, graph[a][neighbour] - residual_network[a][neighbour]), residual_network)
+            cur_delta = dfs_Falk(neighbour, b, min(delta, graph[a][neighbour] - residual_network[a][neighbour]), residual_network)
             
             if cur_delta > 0:
                 residual_network[a][neighbour] += cur_delta
@@ -39,17 +39,13 @@ def dfs(a, b, delta, residual_network):
     return 0
     
 
-def bfs(a, b):
-    pass
-    
-
 def Ford_Falkerson(a, b):
    
     max_flow = 0
     res_network = [[ 0 for i in range(n)] for j in range(n)]
     while True:
         
-        delta = dfs(a, b, math.inf, res_network)
+        delta = dfs_Falk(a, b, math.inf, res_network)
         visited.clear()
                 
         if delta > 0:
@@ -58,26 +54,99 @@ def Ford_Falkerson(a, b):
             print(f"Максимальный поток: {max_flow}")
             return res_network
         
+queue = list()
+dist = [math.inf for i in range(n)]
+
+def dfs_Din(a, b, delta, residual_network):
+    if a == b:
+        return delta
+ 
+    if a in visited:
+        return 0
+ 
+    visited.add(a)
+    
+    for neighbour in [node for node in range(len(graph[a])) if graph[a][node] > 0]:
+
+        if dist[a] == dist[neighbour] + 1:
+            cur_delta = dfs_Din(neighbour, b, min(delta, graph[a][neighbour] - residual_network[a][neighbour]), residual_network)
+            
+            if cur_delta != 0:
+                residual_network[a][neighbour] += cur_delta
+                residual_network[neighbour][a] -= cur_delta 
+                return cur_delta
+    return 0
+    
+def bfs(a, b, res_network):
+    
+    queue.append(a)
+    dist[a] = 0
+    
+    while queue:
+        
+        s = queue.pop(0)
+        for neighbour in [i for i in range(len(graph[s])) if graph[s][i] > 0]:
+            
+            if res_network[s][neighbour] < graph[s][neighbour] and dist[s] != math.inf:
+                dist[neighbour] = dist[a] + 1
+                queue.append(neighbour)
+    
+    return dist[b] != math.inf
+
+
+def Dinica(a, b):
+    
+    res_network = [[ 0 for i in range(n)] for j in range(n)]
+    
+    max_flow = 0
+    while bfs(a, b, res_network):
+        
+        flow = dfs_Din(a, b, 0, res_network.copy())
+        while flow != 0:
+            visited.clear()
+            max_flow += flow
+            flow = dfs_Din(a, b)
+            print(flow)
+            
+        for i in range(n):
+           dist[i] = math.inf
+
+
+        
+    
+    print(f"Максимальный поток по Динице: {max_flow}")
+    return res_network
+
+
+def graph_show(title):
+    
+    G = nx.MultiDiGraph(np.matrix(graph) - np.matrix(edges))
+
+    pos = nx.spiral_layout(G)
+
+    nx.draw_networkx_nodes(G, pos, node_size=700)
+    nx.draw_networkx_edges(G, pos, width=2, alpha = 0.5,  edge_color="b", arrowstyle="->", arrowsize=15, min_target_margin=12)      
+
+    nx.draw_networkx_labels(G, pos, font_size=15, font_family="sans-serif")
+
+    labels = nx.get_edge_attributes(G, 'weight')
+
+    t_labels = dict()
+    for label in labels.keys():
+        t_labels[(label[0], label[1])] = labels[label]
+
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=t_labels)
+
+    plt.title(f"Максимальный поток {title}")
+    plt.show()  
+    
          
 edges = Ford_Falkerson(0, 3)
+graph_show("Фалкерсона")
 
-G = nx.MultiDiGraph(np.matrix(graph) - np.matrix(edges))
+edges = Dinica(0, 3)
+graph_show("Диница")
 
-pos = nx.layout(G, equidistant=True)
 
-nx.draw_networkx_nodes(G, pos, node_size=700)
-nx.draw_networkx_edges(G, pos, width=2, alpha = 0.5,  edge_color="b", arrowstyle="->", arrowsize=15, min_target_margin=12)      
 
-nx.draw_networkx_labels(G, pos, font_size=15, font_family="sans-serif")
-
-labels = nx.get_edge_attributes(G, 'weight')
-
-t_labels = dict()
-for label in labels.keys():
-    t_labels[(label[0], label[1])] = labels[label]
-
-nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=t_labels)
-
-plt.title("Max flow")
-plt.show()  
             
